15d61bc1cf44d85686417ab1adec561160aec6cb
BuiDuc1812 <db939397@gmail.com>
2023-02-26 17:10:29 +0700

    a

--- assets/global.js
+++ assets/global.js
@@ -1101,26 +1101,29 @@ function addToCart(){
 }
 addToCart()
 
-function open(){
-  var accordion = document.querySelectorAll(".accordion");
-  accordion.forEach((item, index)=>{
-    console.log(item)
-    let header = item.querySelector(".accordion-header")
-    console.log(header)
+
+var accordion = document.querySelectorAll(".accordion");
+accordion.forEach((item, index)=>{
+  var header = item.querySelector(".accordion-header")
+  console.log(header);
+  header.addEventListener('click',()=>{
+    item.classList.toggle("open")
+    let description = item.querySelector(".accordion-description");
+    if(item.classList.contains("open")){
+      description.style.height = `${description.scrollHeight}px`
+    }
+    else{
+      description.style.height = "0"
+    }
+    removeOpen(index)
+  })
+})
+function removeOpen(i){
+  accordion.forEach((item, ind)=>{
+    if(i != ind){
+      item.classList.remove("open");
+      let des = item.querySelector(".accordion-description");
+      des.style.height="0";
+    }
   })
 }
-    // header.addEventListener('click',()=>{
-    //   item.classList.toggle("open")
-    //   console.log(item)
-    //   let description = item.querySelector(".accordion-description");
-    //   if(item.classList.contains(open))
-    //     {
-    //       // description.style.height = "90px";
-    //     }
-    //   else
-    //     {
-    //       description.style.height = "0px"
-    //     }
-    //   })
-open();
-
