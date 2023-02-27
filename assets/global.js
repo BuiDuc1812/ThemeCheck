764a03152a4d042b49f73c329ad5d8162685c746
BuiDuc1812 <db939397@gmail.com>
2023-02-27 15:16:18 +0700

    s

--- assets/global.js
+++ assets/global.js
@@ -1100,30 +1100,3 @@ function addToCart(){
   }
 }
 addToCart()
-
-
-var accordion = document.querySelectorAll(".accordion");
-accordion.forEach((item, index)=>{
-  var header = item.querySelector(".accordion-header");
-  console.log(header);
-  header.addEventListener('click',()=>{
-    item.classList.toggle("open");
-    let description = item.querySelector(".accordion-description");
-    if(item.classList.contains("open")){
-      description.style.height = `${description.scrollHeight}px`;
-    }
-    else{
-      description.style.height = "0";
-    }
-    removeOpen(index);
-  })
-})
-function removeOpen(i){
-  accordion.forEach((item, ind)=>{
-    if(i != ind){
-      item.classList.remove("open");
-      let des = item.querySelector(".accordion-description");
-      des.style.height="0";
-    }
-  })
-}
