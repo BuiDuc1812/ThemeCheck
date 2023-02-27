1cc0c1f972e7e84bf40abd6a249875e291b8adbd
BuiDuc1812 <db939397@gmail.com>
2023-02-28 00:20:16 +0700

    s

--- assets/slick-topmenu.js
+++ assets/slick-topmenu.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     $('.slider-header').slick();
   });
+
+  if(screen.width <= 990){
+     $(document).ready(function() {
+        $('.container').slick();
+    });
+  }
+  else
+ 
+  console.log(screen.width);
