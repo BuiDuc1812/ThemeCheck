14edacb3562d7bc7f330938a237f87e49a45764c
BuiDuc1812 <db939397@gmail.com>
2023-03-01 16:53:21 +0700

    a

--- assets/slick-topmenu.js
+++ assets/slick-topmenu.js
@@ -1,22 +1,13 @@
-// $(document).ready(function() {
-//     $('.slider-header').slick({
-//       autoplay:true,
-//       autoplaySpeed:2000
-//     });
-//   });
-//   if($(window).width()<=990){
-//      $(document).ready(function() {
-//         $('.container').slick({
-//           autoplay:true,
-//           autoplaySpeed:2000
-//         });
-//     });
-//   }
 
-window.addEventListener("resize",(event)=>{
-  if(window.width() <= 990){
-    var topMenu = document.querySelector(".container")
-    console.log(topMenu);
-  }  
-})
-onresize = (event) => {};
+onresize = (item)=>{
+  if(screen.width <= 990 ){
+    var swiper = new Swiper(".swiper-slider", {
+      autoplay: {
+        delay: 2000,
+      },
+    });
+  }
+  else{
+  }
+}
+
