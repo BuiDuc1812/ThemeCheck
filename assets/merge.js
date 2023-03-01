109b9c64c6189deccfc96033f439a96ed7c0561c
BuiDuc1812 <db939397@gmail.com>
2023-03-01 20:37:35 +0700

    q

--- assets/merge.js
+++ assets/merge.js
@@ -1,6 +1,15 @@
-var merge = document.querySelector(".mega-menu__list")
-var list = merge.querySelectorAll(".merge")
-list.forEach((item)=>{
-    console.log(item)
-    
+var ul = document.querySelector(".list-girl")
+var li = document.createElement("li")
+ul.appendChild(li)
+var first = ul.getAttribute("first")
+var second = ul.getAttribute("second")
+var list = ul.querySelectorAll(".merge")
+list.forEach((item, index)=>{
+    if(index == first ){
+        li.appendChild(item)
+    }
+    if(index == second ){
+        li.appendChild(item)
+    }
 })
+li.setAttribute("merge","true")
