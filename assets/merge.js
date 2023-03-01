815a2f9bbe4d2ebf6426ec0a24538205f89b4d85
BuiDuc1812 <db939397@gmail.com>
2023-03-01 13:17:58 +0700

    a

--- assets/merge.js
+++
@@ -1,8 +0,0 @@
-// var merge = document.querySelectorAll(".merge");
-// merge.forEach((item, index)=>{
-//     var col2 = item.querySelector(".list-unstyled")
-//     if(index == 1){
-//         item.appendChild(col2)
- 
-//     }                   
-// })    
--- assets/slick-topmenu.js
+++ assets/slick-topmenu.js
@@ -1,14 +1,22 @@
-$(document).ready(function() {
-    $('.slider-header').slick({
-      autoplay:true,
-      autoplaySpeed:2000
-    });
-  });
-  if($(window).width()<=990){
-     $(document).ready(function() {
-        $('.container').slick({
-          autoplay:true,
-          autoplaySpeed:2000
-        });
-    });
-  }
+// $(document).ready(function() {
+//     $('.slider-header').slick({
+//       autoplay:true,
+//       autoplaySpeed:2000
+//     });
+//   });
+//   if($(window).width()<=990){
+//      $(document).ready(function() {
+//         $('.container').slick({
+//           autoplay:true,
+//           autoplaySpeed:2000
+//         });
+//     });
+//   }
+
+window.addEventListener("resize",(event)=>{
+  if(window.width() <= 990){
+    var topMenu = document.querySelector(".container")
+    console.log(topMenu);
+  }  
+})
+onresize = (event) => {};
