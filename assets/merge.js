7ed5b0737c70f6177c523f54451d76d0ab048808
BuiDuc1812 <db939397@gmail.com>
2023-03-02 18:19:16 +0700

    a

--- assets/merge.js
+++ assets/merge.js
@@ -1,20 +1,25 @@
-var ul = document.querySelector(".merge-list");
-var li = document.createElement("li");
-var first = ul.getAttribute("first");
-var second = ul.getAttribute("second");
-var list = ul.querySelectorAll(".merge");
-list.forEach((item, index)=>{
-    if(index == first ){
-        li.appendChild(item);
-    }
-    if(index == second ){
-        li.appendChild(item);
-        item.style.margin = "24px 0 0 0" ;
-    }
-    if(index == first - 1){
-        insertAfter(li,item);
-    }
-})
-function insertAfter(li, element) {
-    element.parentNode.insertBefore(li, element.nextSibling);
-}
+var merge = document.querySelector(".merge-list");
+var newLi = document.createElement("li");
+var first = merge.getAttribute("first");
+var second = merge.getAttribute("second");
+var third = merge.getAttribute("third");
+var fouth = merge.getAttribute("fouth");
+
+console.log(first,second,third,fouth)
+
+// var list = ul.querySelectorAll(".merge");
+// list.forEach((item, index)=>{
+//     if(index == first ){
+//         li.appendChild(item);
+//     }
+//     if(index == second ){
+//         li.appendChild(item);
+//         item.style.margin = "24px 0 0 0" ;
+//     }
+//     if(index == first - 1){
+//         insertAfter(li,item);
+//     }
+// })
+// function insertAfter(li, element) {
+//     element.parentNode.insertBefore(li, element.nextSibling);
+// }
