adeaa203a21623956cb1d7c49aeeecb3e86811b1
BuiDuc1812 <db939397@gmail.com>
2023-03-03 14:54:02 +0700

    a

--- assets/accordion.js
+++ assets/accordion.js
@@ -1,19 +1,19 @@
 
-var accordion = document.querySelectorAll(".accordion");
+var accordion = document.querySelectorAll('.accordion');
 accordion.forEach((item, index)=>{
-  var header = item.querySelector(".accordion-header");
+  var header = item.querySelector('.accordion-header');
   header.addEventListener('click',()=>{
-    item.classList.toggle("open");
-    let description = item.querySelector(".accordion-description");
-    if(item.classList.contains("open")){
-      item.querySelector(".down").classList.add('turn-off');
-      item.querySelector(".up").classList.remove('turn-off');
+    item.classList.toggle('open');
+    let description = item.querySelector('.accordion-description');
+    if(item.classList.contains('open')){
+      item.querySelector('.down').classList.add('turn-off');
+      item.querySelector('.up').classList.remove('turn-off');
       description.style.height = `${description.scrollHeight}px`;
     }
     else{
-      description.style.height = "0";
-      item.querySelector(".down").classList.remove('turn-off');
-      item.querySelector(".up").classList.add('turn-off');
+      description.style.height = '0';
+      item.querySelector('.down').classList.remove('turn-off');
+      item.querySelector('.up').classList.add('turn-off');
     }
     removeOpen(index);
   })
@@ -21,15 +21,15 @@ accordion.forEach((item, index)=>{
 function removeOpen(i){
   accordion.forEach((item, ind)=>{
     if(i != ind){
-      item.classList.remove("open");
-      let des = item.querySelector(".accordion-description");
-      des.style.height="0";
-      item.querySelector(".down").classList.remove('turn-off');
-      item.querySelector(".up").classList.add('turn-off');
+      item.classList.remove('open');
+      let des = item.querySelector('.accordion-description');
+      des.style.height='0';
+      item.querySelector('.down').classList.remove('turn-off');
+      item.querySelector('.up').classList.add('turn-off');
     }
   })
 }
-var swiper = new Swiper(".swiper-drawer", {
+var swiper = new Swiper('.swiper-drawer', {
   autoplay: {
     delay: 2000,
   },
