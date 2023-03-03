613fb19b296714dea46990bd9786216af5947f37
BuiDuc1812 <db939397@gmail.com>
2023-03-03 11:26:38 +0700

    s

--- assets/merge.js
+++ assets/merge.js
@@ -1,23 +1,78 @@
-var parentNodeUl = document.querySelector('.merge-list');
+var parentNodeUl = document.querySelectorAll('.merge-list');
 
-function createLi(){
-    var newLi = document.createElement('li');
-    var liSale = document.querySelector('.li-sale')
-    var first = parentNodeUl.getAttribute('first');
-    var second = parentNodeUl.getAttribute('second');
-    var third = parentNodeUl.getAttribute('third');
-    var fouth = parentNodeUl.getAttribute('fouth');
-    var arr = [first, second, third, fouth]
-    arr.forEach(item=>{
+parentNodeUl.forEach(item=>{
+
+    const createLi = function(){
         var newLi = document.createElement('li');
-        newLi.setAttribute('item',item);
-        newLi.setAttribute('class','new-cols')
-        if (newLi.getAttribute('item') == '0'){
-            newLi.style.display = 'none';
-        }
-        parentNodeUl.insertBefore(newLi,liSale);
-    })
-}
+        var liSale = document.querySelector('.li-sale')
+        var first = item.getAttribute('first');
+        var second = item.getAttribute('second');
+        var third = item.getAttribute('third');
+        var fouth = item.getAttribute('fouth');
+        var arr = [first, second, third, fouth]
+        console.log(arr)
+        arr.forEach(item=>{
+            var newLi = document.createElement('li');
+            newLi.setAttribute('item',item);
+            newLi.setAttribute('class','new-cols')
+            if (newLi.getAttribute('item') == '0'){
+                newLi.style.display = 'none';
+            }
+            item.insertBefore(newLi,liSale);
+        })
+    }
+    createLi();
+    console.log(item)
+    function addItem(){
+        createLi();
+        var newCols = document.querySelectorAll('.new-cols');
+        console.log(newCols)
+        var list = item.querySelectorAll('.merge');
+        let myArray = Array.from(list);
+        newCols.forEach((item) => {
+            let i = 0;
+            var quantityItem = item.getAttribute('item') ;
+            for (const child in myArray) {
+                if (Object.hasOwnProperty.call(myArray, child)) {
+                    const element = myArray[child];
+                    element.style.margin = '0 0 24px 0';
+                    item.appendChild(element);
+                    i++;
+                    if (i >= quantityItem){
+                        break;
+                    }
+                }
+            }
+            for (var x = 0; x < quantityItem; x++){
+                myArray.shift();
+            }
+            if (item.hasChildNodes() == false){
+                item.style.display = 'none';
+            }
+        })
+    }
+    addItem();
+
+})
+
+// function createLi(){
+//     var newLi = document.createElement('li');
+//     var liSale = document.querySelector('.li-sale')
+//     var first = parentNodeUl.getAttribute('first');
+//     var second = parentNodeUl.getAttribute('second');
+//     var third = parentNodeUl.getAttribute('third');
+//     var fouth = parentNodeUl.getAttribute('fouth');
+//     var arr = [first, second, third, fouth]
+//     arr.forEach(item=>{
+//         var newLi = document.createElement('li');
+//         newLi.setAttribute('item',item);
+//         newLi.setAttribute('class','new-cols')
+//         if (newLi.getAttribute('item') == '0'){
+//             newLi.style.display = 'none';
+//         }
+//         parentNodeUl.insertBefore(newLi,liSale);
+//     })
+// }
 
 function addItem(){
     createLi();
