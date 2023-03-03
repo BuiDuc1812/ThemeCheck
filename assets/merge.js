3e471f1d8593edea1099d925b34286113630e2c2
BuiDuc1812 <db939397@gmail.com>
2023-03-03 10:39:54 +0700

    last update

--- assets/merge.js
+++ assets/merge.js
@@ -25,26 +25,26 @@ function addItem(){
     var list = parentNodeUl.querySelectorAll('.merge');
     let myArray = Array.from(list);
     newCols.forEach((item) => {
-    let i = 0;
-    var quantityItem = item.getAttribute('item') ;
-    for (const child in myArray) {
-        if (Object.hasOwnProperty.call(myArray, child)) {
-            const element = myArray[child];
-            element.style.margin = '0 0 24px 0';
-            item.appendChild(element);
-            i++;
-            if (i >= quantityItem){
-                break;
+        let i = 0;
+        var quantityItem = item.getAttribute('item') ;
+        for (const child in myArray) {
+            if (Object.hasOwnProperty.call(myArray, child)) {
+                const element = myArray[child];
+                element.style.margin = '0 0 24px 0';
+                item.appendChild(element);
+                i++;
+                if (i >= quantityItem){
+                    break;
+                }
             }
         }
-    }
-    for (var x = 0; x < quantityItem; x++){
-        myArray.shift();
-    }
-    if (item.hasChildNodes() == false){
-        item.style.display = 'none';
-    }
-})
+        for (var x = 0; x < quantityItem; x++){
+            myArray.shift();
+        }
+        if (item.hasChildNodes() == false){
+            item.style.display = 'none';
+        }
+    })
 }
 
 addItem();
