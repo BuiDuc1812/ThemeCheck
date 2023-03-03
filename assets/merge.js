e696dd6ba3a88d87723e1936db0dac54d7eb72d0
BuiDuc1812 <db939397@gmail.com>
2023-03-03 13:32:43 +0700

    a

--- assets/merge.js
+++ assets/merge.js
@@ -1,12 +1,11 @@
 var parentNodeUl = document.querySelectorAll('.merge-list');
 parentNodeUl.forEach(item=>{
-    createLi(item)
-    // addItem(item);
+    addItem(item);
 })
 
 function createLi(params){
     var newLi = document.createElement('li');
-    var liSale = document.querySelector('.li-sale')
+    var liSale = params.querySelector('.li-sale')
     var first = params.getAttribute('first');
     var second = params.getAttribute('second');
     var third = params.getAttribute('third');
@@ -20,34 +19,34 @@ function createLi(params){
         if (newLi.getAttribute('item') == '0'){
             newLi.style.display = 'none';
         }
-        params.insertBefore(newLi,liSale);
+        params.insertBefore(newLi, liSale);
     })
 }
 
-// function addItem(params){
-//     createLi();
-//     var newCols = document.querySelectorAll('.new-cols');
-//     var list = params.querySelectorAll('.merge');
-//     let myArray = Array.from(list);
-//     newCols.forEach((item) => {
-//         let i = 0;
-//         var quantityItem = item.getAttribute('item') ;
-//         for (const child in myArray) {
-//             if (Object.hasOwnProperty.call(myArray, child)) {
-//                 const element = myArray[child];
-//                 element.style.margin = '0 0 24px 0';
-//                 item.appendChild(element);
-//                 i++;
-//                 if (i >= quantityItem){
-//                     break;
-//                 }
-//             }
-//         }
-//         for (var x = 0; x < quantityItem; x++){
-//             myArray.shift();
-//         }
-//         if (item.hasChildNodes() == false){
-//             item.style.display = 'none';
-//         }
-//     })
-// }
+function addItem(params){
+    createLi(params);
+    var newCols = params.querySelectorAll('.new-cols');
+    var list = params.querySelectorAll('.merge');
+    let myArray = Array.from(list);
+    newCols.forEach((item) => {
+        let i = 0;
+        var quantityItem = item.getAttribute('item') ;
+        for (const child in myArray) {
+            if (Object.hasOwnProperty.call(myArray, child)) {
+                const element = myArray[child];
+                element.style.margin = '0 0 24px 0';
+                item.appendChild(element);
+                i++;
+                if (i >= quantityItem){
+                    break;
+                }
+            }
+        }
+        for (var x = 0; x < quantityItem; x++){
+            myArray.shift();
+        }
+        if (item.hasChildNodes() == false){
+            item.style.display = 'none';
+        }
+    })
+}
