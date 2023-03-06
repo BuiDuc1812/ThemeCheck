5eb215fa39eb7da7eca11198f92033de67509e10
BuiDuc1812 <db939397@gmail.com>
2023-03-06 11:18:33 +0700

    style rbt

--- assets/swiper-banner.js
+++ assets/swiper-banner.js
@@ -1,23 +1,23 @@
-// var init = false;
-// var swiper = '';
+var init = false;
+var swiper = '';
 
-// function enableSwiper() {
-//   if (window.innerWidth < 990) {
-//     if (!init) {
-//       init = true;
-//       swiper = new Swiper(".swiper-slider", {
-//         autoplay: {
-//           delay: 2000,
-//         },
-//       });
-//     }
-//   } else if (init) {
-//     swiper.destroy();
-//     init = false;
-//   }
-// }
-// enableSwiper();
-// window.addEventListener('resize', enableSwiper)
+function enableSwiper() {
+  if (window.innerWidth < 990) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper-slider", {
+        autoplay: {
+          delay: 2000,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+enableSwiper();
+window.addEventListener('resize', enableSwiper)
 
 
   
