193e2d3ac2e75db93ca5948744fedfe84a03f6c4
BuiDuc1812 <db939397@gmail.com>
2023-03-06 15:34:29 +0700

    add slick

--- assets/swiper-banner.js
+++ assets/swiper-banner.js
@@ -1,6 +1,27 @@
-var swiper = new Swiper(".swiper-rbt", {
-    autoplay: {
-        delay: 2000,
-    },
-});
+var init = false;
+var swiper = '';
 
+function enableSwiper() {
+  if (window.innerWidth < 990) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper-rbt", {
+        // spaceBetween: 30,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction:false,
+                pauseOnMouseEnter:true
+            },
+            loop :true
+        });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+enableSwiper();
+window.addEventListener('resize', enableSwiper)
+
+
+  
