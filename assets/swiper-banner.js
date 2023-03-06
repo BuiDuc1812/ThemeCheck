c8a2cc36531a224c42921037c84e7e85909e2472
BuiDuc1812 <db939397@gmail.com>
2023-03-06 10:52:03 +0700

    fix bug

---
+++ assets/swiper-banner.js
@@ -0,0 +1,23 @@
+// var init = false;
+// var swiper = '';
+
+// function enableSwiper() {
+//   if (window.innerWidth < 990) {
+//     if (!init) {
+//       init = true;
+//       swiper = new Swiper(".swiper-slider", {
+//         autoplay: {
+//           delay: 2000,
+//         },
+//       });
+//     }
+//   } else if (init) {
+//     swiper.destroy();
+//     init = false;
+//   }
+// }
+// enableSwiper();
+// window.addEventListener('resize', enableSwiper)
+
+
+  
