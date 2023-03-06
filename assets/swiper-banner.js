7d08b8dccc55aeb02ee38c7a917703c8ef47651f
BuiDuc1812 <db939397@gmail.com>
2023-03-06 21:27:03 +0700

    add banner img

--- assets/swiper-banner.js
+++ assets/swiper-banner.js
@@ -6,7 +6,10 @@ function enableSwiper() {
     if (!flag) {
         flag = true;
         swiperRtb = new Swiper(".swiper-rbt", {
+            centeredSlides: true,
             loop: true,
+            slidesPerView: 'auto',
+            speed: 3000,
             autoplay: {
                 delay: 0,
                 disableOnInteraction: false,
