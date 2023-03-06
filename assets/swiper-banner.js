e11071f36f173c6ffa1f9b0530a4c0e9d10e1d3a
BuiDuc1812 <db939397@gmail.com>
2023-03-06 16:25:52 +0700

    add img banner

--- assets/swiper-banner.js
+++ assets/swiper-banner.js
@@ -1,27 +1,27 @@
-var init = false;
-var swiper = '';
+// var init = false;
+// var swiper = '';
 
-function enableSwiper() {
-  if (window.innerWidth < 990) {
-    if (!init) {
-      init = true;
-      swiper = new Swiper(".swiper-rbt", {
-        // spaceBetween: 30,
-            autoplay: {
-                delay: 0,
-                disableOnInteraction:false,
-                pauseOnMouseEnter:true
-            },
-            loop :true
-        });
-    }
-  } else if (init) {
-    swiper.destroy();
-    init = false;
-  }
-}
-enableSwiper();
-window.addEventListener('resize', enableSwiper)
+// function enableSwiper() {
+//   if (window.innerWidth < 990) {
+//     if (!init) {
+//       init = true;
+//       swiper = new Swiper(".swiper-rbt", {
+//         // spaceBetween: 30,
+//             autoplay: {
+//                 delay: 0,
+//                 disableOnInteraction:false,
+//                 pauseOnMouseEnter:true
+//             },
+//             loop :true
+//         });
+//     }
+//   } else if (init) {
+//     swiper.destroy();
+//     init = false;
+//   }
+// }
+// enableSwiper();
+// window.addEventListener('resize', enableSwiper)
 
 
   
