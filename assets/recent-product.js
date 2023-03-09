cd68e845cbeef52e580f20c5ee81266e2088f869
BuiDuc1812 <db939397@gmail.com>
2023-03-09 18:10:58 +0700

    fix js

--- assets/recent-product.js
+++ assets/recent-product.js
@@ -1,40 +1,30 @@
+var countPaginateViewed = document.querySelector('.recently-viewed')
+var unnitPerView = countPaginateViewed.getAttribute('paginate')
+
 const localViewed = localStorage.recentlyViewedProduct;
 function getRecentlyViewedProducts() {
-  const productData = JSON.parse(localStorage.getItem("recentlyViewedProduct"));
+  const productData = JSON.parse(localStorage.getItem('recentlyViewedProduct'));
   const recentlyViewedHtml = [];
   productData.map(item => {
     recentlyViewedHtml.unshift(`
-    <li class="grid__item">
-     <div class="card-wrapper underline-links-hover">
-      <div class="card card--standard card--media " style="--ratio-percent: 100%;">
-        <div class="card__inner color-background-2 gradient ratio" style="--ratio-percent: 100%;">
-         <div style="position:static" class="card__media">
-         <div class="media media--transparent media--hover-effect">
-    		  <img class="motion-reduce" src="${item.productImg}" width="${item.imgWidth}" height="${item.imgHeight}"  loading="lazy" alt="${item.productImageAltText}"/>
-         </div>
-         </div>
-       </div>
-       <div class="card__content">
-       <div class="card__information">
-       <h3 class="card__heading h5">
-       <a class="full-unstyled-link" href="${item.productUrl}">${item.productTitle}</a></h3>
-         <div class="card-information">
-           <div class="price ">
-             <div class="price__container">
-               <div class="price-item price-item--regular">
-                   ${item.productPrice}
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
-       </div>
-       </div>
-       </div>
-    </li>
-   `);
+    <div class="swiper swiper-recently-viewed">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide viewed-item">
+                <div class="viewed-img">
+                    <img loading="lazy" src="${item.productImg}" alt="">
+                </div>
+                <div class="viewed-info">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
   });
   const newProductData = `${recentlyViewedHtml.join("")}`;
   const fullContent = document.getElementsByClassName("recently-viewed");
   fullContent[0].innerHTML = newProductData;
 }
+document.addEventListener("DOMContentLoaded", function (event) {
+    getRecentlyViewedProducts();
+});
