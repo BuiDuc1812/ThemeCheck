bf3ce84cb38b523ee5aca59471e9244d6c07eff9
BuiDuc1812 <db939397@gmail.com>
2023-03-10 17:02:18 +0700

    last update

--- assets/recent-product.js
+++ assets/recent-product.js
@@ -17,24 +17,27 @@ function getRecentlyViewedProducts() {
     productData.map(item => {
     recentlyViewedHtml.unshift(`
         <div class="swiper-slide viewed-item">
-            <div class="viewed-img">
-                <img loading="lazy" src="${item.productImg}" alt="">
+            <a href="${item.productUrl}" class="viewed-img">
+                <img loading="lazy" src="${item.productImg}" alt="${item.productImageAltText}">
                 <span class="tag-product">${item.productTag}</span>
                 <span class="tag-sale">10% off everyday</span>
-            </div>
+                <div class="tag-search"></div>
+            </a>
             <div class="viewed-info">
                 <a href="${item.productUrl}">${item.productTitle}</a> 
                 <p>${item.productVendor}</p> 
                 <div>
-                  <span class="product-price">${item.productPrice}</span>
-                  <span class="product-price-compare">RRP ${item.productPriceCompare}</span>
+                  ${ item.productPriceCompare ? 
+                    `<span class="product-price">${item.productPrice}</span>
+                    <span class="product-price-compare">RRP ${item.productPriceCompare}</span>`
+                    :`<span class="product-price">${item.productPrice}</span>`}  
                 </div>
             </div>
         </div>
       `);
     });
   }
-  const newProductData = `${recentlyViewedHtml.join("")}`;
+  const newProductData = `${recentlyViewedHtml.join('')}`;
   const fullContent = document.getElementsByClassName('product-viewed');
   fullContent[0].innerHTML = newProductData;
   if(productData.length <= 4){
@@ -48,22 +51,25 @@ function getRecentlyViewedProducts() {
     viewedProduct.style.gap='0';
     var swiperProduct = new Swiper('.swiper-recently-viewed', {
       breakpoints: {
-        750: {
+        990: {
           slidesPerView: unitPerView,
           spaceBetween: 16
         },
-
+        750: {
+          slidesPerView: 3,
+          spaceBetween: 16
+        },
         320: {
           slidesPerView: 2,
           spaceBetween: 8
         }
       },
         navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
         pagination: {
-          el: ".swiper-pagination",
+          el: '.swiper-pagination',
         }
     });
   }
